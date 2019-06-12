c74e72c0069cd98033231353d0d0a54041b453c7
Hammad Malik <tomatohammado@gmail.com>
2019-06-12 16:44:42 -0400

    Rename file, add content
    - yes this should be two commits, sue me

---
+++ create-prettier-md-config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LCYAN='\033[1;36m'
+NC='\033[0m' # No Color
+
+echo -e "${Yellow}Creating `.prettierrc`... ${NC}"
+
+touch .prettierrc
+
+echo '{
+  "proseWrap": "always"
+}
+' >> .prettierrc
+
+echo -e "${GREEN}Done! ${NC}"
